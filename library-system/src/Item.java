235e79e210d533f94931341cb0cb4855538d3cd0
zakiicode <92188960+zakiicode@users.noreply.github.com>
2024-03-19 16:28:43 -0400

    Update Item.java

--- library-system/src/Item.java
+++ library-system/src/Item.java
@@ -1,5 +1,7 @@
 
 /* Abstract class describing an Item object. It defines several attributes which are inherited by all subclasses of Item.
+*
+*Zaki: added getters and setters as well as an empty constructor which is needed for the database class
  */
 public abstract class Item {
 	static int IDS = 0;
@@ -12,6 +14,41 @@ public abstract class Item {
 		id = ++IDS;
 		this.name = name;
 		this.type = type;
+		this.price = price;
+	}
+	public Item() {
+		super();
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
 		this.price = price;
 	}
 }
