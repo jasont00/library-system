fd124b9a73de4d1052d25ac87fbe771e6d4df54e
Jason <91354148+jasont00@users.noreply.github.com>
2024-03-19 16:35:00 -0400

    added Course class
    simple skeleton, doesn't do much currently
    
    attributes:
    - name
    - course code (denoted "code")
    - textbooks (list)
    
    methods:
    - constructor
    - getTextbooks() returns list of textbooks

---
+++ library-system/src/Course.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+/* Class that represents a course and has a list of textbooks. Is associated with the Textbook class.
+ */
+public class Course {
+	String name;
+	String code;
+	ArrayList<String> textbooks;
+	
+	public Course(String n, String c) {
+		name = n;
+		code = c;
+		
+		textbooks = new ArrayList<String>();
+	}
+	
+	public ArrayList<String> getTextbooks() {
+		return textbooks;
+	}
+	
+}
