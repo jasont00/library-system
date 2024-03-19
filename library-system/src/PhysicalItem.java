6f1116aead7dc8172e0bed05965f443fa46fb5fe
zakiicode <92188960+zakiicode@users.noreply.github.com>
2024-03-19 16:29:50 -0400

    Update PhysicalItem.java

--- library-system/src/PhysicalItem.java
+++ library-system/src/PhysicalItem.java
@@ -1,5 +1,6 @@
 /* Concrete class PhysicalItem extends Item and represents a physical item that is stored in the library.
  * Unique attribute rentable is a boolean that denotes whether or not the item is available or able to be rented. Admins are able to change this.
+ * Zaki: added constructor for databse as well as a to string
  */
 public class PhysicalItem extends Item{
 	boolean rentable;
@@ -8,5 +9,11 @@ public class PhysicalItem extends Item{
 		super(name, type, price);
 		this.rentable = rentable;
 	}
-	
+	public PhysicalItem() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", id=" + id + ", type=" + type + ", price=" + price + "]";
+	}
 }
