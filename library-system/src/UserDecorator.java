a5e8d01c2d1a79959e1ae9cffde5c14aac1b0f3f
Jayznn <101909629+Jayznn@users.noreply.github.com>
2024-03-20 11:55:20 -0400

    Update UserDecorator.java

--- library-system/src/UserDecorator.java
+++ library-system/src/UserDecorator.java
@@ -1,5 +1,12 @@
 package librarysystem;
 
 public class UserDecorator extends UserBuilder{
-
+	public UserDecorator(User u) {
+		super(u);
+	}
+	
+	@Override
+	public void assemble() {
+		super.assemble();
+	}
 }
