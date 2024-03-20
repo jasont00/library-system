24000f5daf6edbc23844463bc6c89859bb025e52
Sharon-Sunny <91921059+Sharon-Sunny@users.noreply.github.com>
2024-03-20 15:59:39 -0400

    Updated Search
    Includes all of the methods which checks the item the User searched for and also similar items in the database if it exists

--- library-system/src/Search.java
+++ library-system/src/Search.java
@@ -1,54 +1,80 @@
-package librarysystem;
+package library;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Search {
+
 	
-//	public void search() {
-//		//prompt user to search somehow? get input then call filter in this method
-//		--> delete since we do not need this
-//	}
-	public Boolean filter() {
+	public void search() {
 		//return a statement asking if user wants to use the filter /
 		Scanner input = new Scanner(System.in);
 		System.out.println("Use the filter? Yes or No"); 
 		String in = input.nextLine(); 
 		
+		// if the user asks for a filter then the search is filtered by type and name and accesses the filterSearch method with the 2 parameters
+		if (in.equalsIgnoreCase("yes")) {
+			Scanner type = new Scanner(System.in);
+			System.out.println("Enter the type of the Item (Online Item or Physical)");
+			String itemType = type.nextLine();
+			type.close();
+			Scanner name = new Scanner(System.in);
+			System.out.println("Enter the name of the item");
+			String key = name.nextLine(); 
+			name.close(); 
+			filterSearch(key, itemType); 
+		}
+		
+		else if (in.equalsIgnoreCase("no")) {
+			Scanner name = new Scanner(System.in);
+			System.out.println("Enter the name of the item");
+			String key = name.nextLine(); 
+			name.close(); 
+			search(key); 
+		}
 		
 		
 		
-		return true;
 	}
 //	--> can do it through Test Cases till the GUI is implemented which then this would prompt user for result through sys.out
 	
-	public String filterSearch(String key) {
+	public Item filterSearch(String key, String itemType) {
+		Item item = new Item(); 
+		item.name = key; 
+		item.type = itemType; 
 		
-		
-		return null; 
+		if (Database.checkStock(item)> 0) {
+			return Item.getItem(key); 
+		}
+		else return null;  
 	}
-	public ArrayList<String> search(String key) {
-		// access database
+	public Item search(String key) {
 		
-		if (checkStock(key)>1) {
+		if (Database.checkStock(key)>0) {
+			return Item.getItem(key); 
+		}
+		
+		if (Database.checkStock(key)>1) {
 			ArrayList<String> similarList = checkSimilar(key); 
 			//return options
 		}
-		else return "This item is currently Not in Stock";
+		else return null; 
 				
-		return null; 	
+ 	
 	}
 	
 	public ArrayList<String> checkSimilar(String key){
-		//words that are less than 3-5 letters different than key is considered "similar"
+	//words that are less than 3-5 letters different than key is considered "similar"
+
 		
 		ArrayList<String> similarItem = new ArrayList<String>;
-		for (String word: Inventory) //inventory is a list in the library databse
+		for (String word: Database.items) //items is a list in the library database
 			if(levenshteinDistance(key, word) <= 5) {
 				similarItem.add(word); 
 			}
 		
-		return null;	
+		return similarItem;	
+		
 	}
 	public int levenshteinDistance(String key, String word) {
 		int[][]dist = new int[word.length()+1][word.length()+1]; 
@@ -76,7 +102,4 @@ public class Search {
 
 }
 
-class testSearch extends Search {
-	
-}
 
