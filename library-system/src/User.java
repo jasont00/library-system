663385329b05f9f7e26ddde51f1b21414f7d09ca
Jayznn <101909629+Jayznn@users.noreply.github.com>
2024-03-20 11:03:13 -0400

    Update User.java

--- library-system/src/User.java
+++ library-system/src/User.java
@@ -1,10 +1,22 @@
 package librarysystem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class User {
 	
 	private String type = "";
 	private String email = "";
 	private String password = "";
+	public boolean rentEligible;
+	List<Item> rentedItems = new ArrayList<>();
+	List<Newsletter> subscriptions = new ArrayList<>();
+	public List<Item> purchasedItems = new ArrayList<>();
+	
+	public void viewTextbooks() {
+		// TODO Auto-generated method stub
+		
+	}
 
 	public void setType(String s) {
 		this.type = s;
@@ -18,35 +30,31 @@ class User {
 	public void setPassword(String s) {
 		this.password = s;
 	}
-
+	
 	@Override
 	public void rentItem(Item i) {
-		// TODO Auto-generated method stub
-		
+		rentedItems.add(i);
 	}
-
+	
 	@Override
 	public void subscribe(Newsletter n) {
-		// TODO Auto-generated method stub
-		
+		subscriptions.add(n);
 	}
 
 	@Override
 	public void cancel(Newsletter n) {
-		// TODO Auto-generated method stub
-		
+		subscriptions.remove(n);
 	}
 
-	@Override
 	public void requestItem(String s) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	
 	@Override
 	public void purchase(Item i) {
-		// TODO Auto-generated method stub
-		
+		purchasedItems.add(i);
+		getPrice(i.getName);
 	}
-	
 }
