eb43cd891db2908976fdde39ab4d760f2b6194fb
zakiicode <92188960+zakiicode@users.noreply.github.com>
2024-03-21 12:08:10 -0400

    Update ItemFactory.java

--- library-system/src/ItemFactory.java
+++ library-system/src/ItemFactory.java
@@ -1,37 +1,26 @@
 package librarysystem;
 
-import com.group.librarysystemgui.Model.Item;
-
 /* Concrete factory class responsible for creating the concrete subsclasses of the Item class.
  * *note: should publisher be universal for all items? should online items have ids?
  */
-public class ItemFactory { // c signifies the class
-	public Item getItem(String c, String name, String type, double price, String publisher, boolean rentable) {
-		
-		if (c.equalsIgnoreCase("onlineitem")) {
+public class ItemFactory {
+	public Item getItem(String name, String type, double price, String publisher, boolean rentable) {
+		if (type.equalsIgnoreCase("onlineitem")) {
 			return new OnlineItem(name, type, price, publisher);
 		}
-		
-		else if (c.equalsIgnoreCase("physicalitem")) {	// automatically stores the item in the physicalItemRepo 
-			
+		else if (type.equalsIgnoreCase("physicalitem")) {
 			if (PhysicalItemRepo.itemOccurrences.get(name) > 20) {	// if there's already 20 items with the same name, return void;
 				System.out.println("There are already 20 copies of an item with the same name.");
 				return null;
 			}
-			
-			else {
-				return new PhysicalItem(name, type, price, rentable);
-			}
+			return new PhysicalItem(name, type, price, rentable);
 		}
-		
-		else if (c.equalsIgnoreCase("textbook")) {
+		else if (type.equalsIgnoreCase("textbook")) {
 			return new Textbook(name, type, price, publisher);
 		}
-		
-		else if (c.equalsIgnoreCase("newsletter")) {
+		else if (type.equalsIgnoreCase("newsletter")) {
 			return new Newsletter(name, type, price, publisher);
 		}
-		
 		else {
 			return null;
 		}
