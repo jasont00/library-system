f23782dc312cabbdc3f7485cfddf4902eca9ee1d
Jayznn <101909629+Jayznn@users.noreply.github.com>
2024-03-21 10:43:14 -0400

    Update FacultyBuilder.java

--- library-system/src/FacultyBuilder.java
+++ library-system/src/FacultyBuilder.java
@@ -1,28 +1,7 @@
 package librarysystem;
 
-class FacultyBuilder extends UserDecorator {
-	
+class FacultyBuilder extends UserBuilder {
 
-	public void Courses() { //added bahaviour for decorator pattern
-		// TODO Auto-generated method stub
-		
-	}
-	
-	public void viewTextbooks() { //added bahaviour for decorator pattern
-		// TODO Auto-generated method stub
-		
-	}
-	
-	public void update(TextbookSubject t) { //added bahaviour for decorator pattern
-		// TODO Auto-generated method stub
-	}
-	
-	@Override
-	public void assemble() {
-		super.assemble();
-		System.out.println("Faculty Features");
-	}
-	
 	public void buildType() {
 		user.setType("faculty");
 	}
@@ -33,33 +12,6 @@ class FacultyBuilder extends UserDecorator {
 
 	public void buildPassword() {
 		user.setPassword("facultysPassword");
-	}
-
-	@Override
-	public void rentItem(Item i) {
-		rentedItems.add(i);
-	}
-
-	@Override
-	public void subscribe(Newsletter n) {
-		subscriptions.add(n);
-	}
-
-	@Override
-	public void cancel(Newsletter n) {
-		subscriptions.remove(n);
-	}
-
-	@Override
-	public void requestItem(String s) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void purchase(Item i) throws Exception {
-		purchasedItems.add(i);
-		PaymentHandler.getPaymentHandler().getPrice(i.getName());
 	}
 	
 }
