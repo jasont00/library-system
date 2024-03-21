211e9d3b375dc857383d2ef1968aea0c0918a80a
zakiicode <92188960+zakiicode@users.noreply.github.com>
2024-03-21 19:19:39 -0400

    Update Newsletter.java

--- library-system/src/Newsletter.java
+++ library-system/src/Newsletter.java
@@ -4,10 +4,10 @@ public class Newsletter extends OnlineItem {
 	// price represents the subscription price (per month)
 	// some external features that are outside the scope of this course should be added here to accomodate viewing the newsletter through the external "interface"
 	
-	public Newsletter (String name, double price, String publisher) {
+	public Newsletter (String name, double price, String publisher)throws Exception {
 		super(name, "newsletter", price, publisher);
 	}
-	public Newsletter (String name, String type, double price, String publisher) {
+	public Newsletter (String name, String type, double price, String publisher) throws Exception{
 		super(name, "newsletter", price, publisher);
 	}
 	
