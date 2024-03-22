03b3a3bc1cb5db390f28281cc4d224da49b83579
Jason <91354148+jasont00@users.noreply.github.com>
2024-03-22 17:21:59 -0400

    Delete librarysystemgui directory
    we were testing something lol

--- librarysystemgui/Model/CD.java
+++
@@ -1,8 +0,0 @@
-package com.group.librarysystemgui.Model;
-
-public class CD extends PhysicalItem{
-    public CD(String name, double price, boolean rentable) {
-        super(name,price,rentable);
-        super.setType("cd");
-    }
-}
--- librarysystemgui/Model/Magazine.java
+++
@@ -1,9 +0,0 @@
-package com.group.librarysystemgui.Model;
-
-public class Magazine extends PhysicalItem{
-
-    public Magazine(String name, double price, boolean rentable) {
-        super(name,price,rentable);
-        super.setType("magazine");
-    }
-}
