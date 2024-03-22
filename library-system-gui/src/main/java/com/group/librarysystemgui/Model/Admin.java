3e1c3988e6ed6b33d5280adfa5b214f00fee792c
zakiicode <92188960+zakiicode@users.noreply.github.com>
2024-03-22 17:39:54 -0400

    Update Admin.java

--- library-system-gui/src/main/java/com/group/librarysystemgui/Model/Admin.java
+++ library-system-gui/src/main/java/com/group/librarysystemgui/Model/Admin.java
@@ -16,22 +16,4 @@ public class Admin {
     public void addUser(String type, String email, String password)throws Exception {
         User user = new User(type,email,password);
     }
-}package com.group.librarysystemgui.Model;
-
-public class Admin {
-
-    public void enableItem(PhysicalItem item) {
-        item.rentable = true;
-    }
-
-    public void disableItem(PhysicalItem item) {
-        item.rentable = false;
-    }
-    public void createItem(String ctype, String name, String type, double price, String publisher, boolean rentable) throws Exception {
-        ItemFactory factory = new ItemFactory();
-        factory.getItem(ctype, name, price, publisher, rentable);
-    }
-    public void addUser(String type, String email, String password)throws Exception {
-        User user = new User(type,email,password);
-    }
 }
