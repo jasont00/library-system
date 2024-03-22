c8885053ffbdc200cc36141cf11cb3a33e45c40a
zakiicode <92188960+zakiicode@users.noreply.github.com>
2024-03-21 21:41:33 -0400

    Update Databasetest.java

--- library-system/src/Databasetest.java
+++ library-system/src/Databasetest.java
@@ -13,32 +13,33 @@ class Databasetest {
 		String path2 = "D:\\YORK\\EECS 3311\\CSV_Example (1)\\CSV_Example\\user.csv";
 
 		Database maintain = Database.getDatabase();
+		//maintain.updateuser();
+
 		PaymentHandler handle = PaymentHandler.getPaymentHandler();
 		ItemFactory fact = new ItemFactory();
+		PhysicalItemRepo rep = new PhysicalItemRepo();
 		//UserBuilder build = new UserBuilder(use);
-		fact.getItem("gatsby", "onlineitem", 19.99, "me", false);
-		fact.getItem("titanic", "physicalitem", 19.99, "me", false);
+		fact.getItem("onlineitem", "gatsby", "newsletter", 19.99, "me", false);
+		fact.getItem("physicalitem", "titanic", "textbook", 19.99, "me", false);
 		//direc.setAbstractUser(abs);
-		AbstractUser abs = new UserBuilder();
-		AbstractUser abst = new UserBuilder();
+		UserBuilder abs = new basicUserBuilder();
+		UserBuilder abst = new basicUserBuilder();
 		UserDirector direc = new UserDirector();
 		UserDirector direce = new UserDirector();
 
-		direc.setAbstractUser(abs);
+		direc.setUserBuilder(abs);
 		direc.createUser();
 		User use = direc.getUser();
 		use.setEmail("email@gmail.com");
 		use.setPassword("pass");
-		use.setType("Student");
-		maintain.updateuser(path2);
-		direce.setAbstractUser(abst);
+		use.setType("Faculty");
+		//maintain.updateuser();
+		direce.setUserBuilder(abst);
 		direce.createUser();
 		User usee = direce.getUser();
 		usee.setEmail("email@gmail.com");
 		usee.setPassword("Pass66");
 		usee.setType("Student");
-		System.out.println(use.getPassword());
-
 
 		for(Item u: maintain.items){ 
 			System.out.println(u.toString());
@@ -48,16 +49,15 @@ class Databasetest {
 		}
 		
 		
-		maintain.updateitem(path);
-		maintain.updateuser(path2);
+		//maintain.updateitem();
+		//maintain.updateuser();
 
-		System.out.println(maintain.search(path, "gatsby","search"));
+		System.out.println(maintain.search("titanic","search"));
 		//Assert.assertEquals("Search result: User [name=gatsby, id=1, type=book, price=19.99]",maintain.search(path, "gatsby","search"));
 		Assert.assertEquals(19.99,handle.getPrice("gatsby"),0);
 		System.out.println(PaymentHandler.getPaymentHandler().getPrice("gatsby"));
-		System.out.println(maintain.checkstock(path,"gatsby"));
+		System.out.println(maintain.checkstock("gatsby"));
 
 	}
 
 }
-
