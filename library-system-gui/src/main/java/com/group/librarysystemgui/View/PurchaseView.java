a763141c8b7359116952b7841133e94df1946840
zakiicode <92188960+zakiicode@users.noreply.github.com>
2024-03-22 15:53:16 -0400

    Update PurchaseView.java

--- library-system-gui/src/main/java/com/group/librarysystemgui/View/PurchaseView.java
+++ library-system-gui/src/main/java/com/group/librarysystemgui/View/PurchaseView.java
@@ -1,6 +1,7 @@
 package com.group.librarysystemgui.View;
 
 import com.group.librarysystemgui.Controller.PaymentHandler;
+import com.group.librarysystemgui.Model.Database;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
