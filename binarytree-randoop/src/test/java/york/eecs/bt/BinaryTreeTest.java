c0796b0c649c2d1029de395a17b2c83a4a11fe2a
Jason <91354148+jasont00@users.noreply.github.com>
2024-04-06 17:16:27 -0400

    uploaded randoop tutorial

---
+++ binarytree-randoop/src/test/java/york/eecs/bt/BinaryTreeTest.java
@@ -0,0 +1,19 @@
+package york.eecs.bt;
+
+import york.eecs.bt.BinaryTree;
+import york.eecs.bt.Node;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class BinaryTreeTest {
+
+    @Test
+    public void testBinaryTree() throws Exception {
+    	BinaryTree tree;
+    	Node root = new Node(1);
+        tree = new BinaryTree(root);
+    }
+    
+}
