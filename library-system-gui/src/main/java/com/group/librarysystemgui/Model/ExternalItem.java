e1c1524f9d2767a03452bc2ca846372a28c880bc
Sharon-Sunny <91921059+Sharon-Sunny@users.noreply.github.com>
2024-04-07 16:15:45 -0400

    Update ExternalItem.java
    changed constructors that were adding the discount incorrectly (returned discount percentage rather than discounted amount from original price)

--- library-system-gui/src/main/java/com/group/librarysystemgui/Model/ExternalItem.java
+++ library-system-gui/src/main/java/com/group/librarysystemgui/Model/ExternalItem.java
@@ -8,16 +8,16 @@ public class ExternalItem {
     // constructor takes discount (0-1) and original price before discount
     // if the given discount is greater than 1, it automatically assigns discount as 1.
     public ExternalItem(double d, double p) {
-        this.discount = 0<d && d<1 ? d : 1 ;
-        price = p;
+        this.discount = 0<d && d<1 ? d : 0 ;
+        price = p; 
     }
-    public void setDiscount(double discount) {
-        this.discount = discount;
+    public void setDiscount(double d) {
+    	this.discount = 0<d && d<1 ? d : 0 ;
     }
-
+ 
     // returns discounted price
     public double getPrice() {
-        return this.price * this.discount;
+        return this.price - (this.price * this.discount); 
     }
 
 }
