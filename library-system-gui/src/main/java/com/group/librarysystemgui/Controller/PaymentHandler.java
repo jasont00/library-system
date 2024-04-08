b00ee7c3cd988ac65aac06c5199e4a026b13dbd4
Sharon-Sunny <91921059+Sharon-Sunny@users.noreply.github.com>
2024-04-08 13:45:09 -0400

    Update PaymentHandler.java
    Edited so that the discounted prices were returned rather than just the discount values

--- library-system-gui/src/main/java/com/group/librarysystemgui/Controller/PaymentHandler.java
+++ library-system-gui/src/main/java/com/group/librarysystemgui/Controller/PaymentHandler.java
@@ -6,10 +6,11 @@ public class PaymentHandler {
     public static Double getPrice(String name, String discount) throws  Exception {
         double d = Double.parseDouble(discount);
         if(d<0 || d>1){
-            d = 1;
-        }
+            d = 0;
+        } 
         Double price = Double.valueOf(Database.getDatabase().search(name, "pay"));
-        price = price*d;
+        price = price - (price*d);
         return price;
+    }
     }
 }
